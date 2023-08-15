c006b1988a7b58e6c0841345ab6119fea44bd4b1
GoutamrajSahu <sahugoutamraj@gmail.com>
2023-08-15 22:07:10 +0530

    Initial commit from GRS

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + " ---Colide with other--- " + other.gameObject.name);
-    }
+    [SerializeField] float LoadDelay = 1.0f;
+    /* void OnCollisionEnter(Collision other)
+     {
+         Debug.Log(this.name + " ---Colide with other--- " + other.gameObject.name);
+     }*/
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log( $"{this.name} ***Colide with other*** {other.gameObject.name}");
+        // Debug.Log( $"{this.name} ***Colide with other*** {other.gameObject.name}");
+        StartCrashSequence();
+    }
 
+    private void StartCrashSequence()
+    {
+       GetComponent<PlayerController>().enabled = false;
+       Invoke("ReloadLevel1", LoadDelay);
+    }
+
+    void ReloadLevel1()
+    {
+        int curentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(curentSceneIndex);
     }
 }
