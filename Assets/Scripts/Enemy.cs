420b10093632e80e41af1a7419dbb315c76585bc
GoutamrajSahu <sahugoutamraj@gmail.com>
2023-08-22 23:20:23 +0530

    Add enemies prefab.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int hitPoints = 7;
 
     ScoreBoard scoreBoard;
-    private int scorePerHit = 15;
+    [SerializeField] private int scorePerHit = 15;
 
     void Start()
     {
-       scoreBoard = FindAnyObjectByType<ScoreBoard>();   
+        scoreBoard = FindAnyObjectByType<ScoreBoard>();
+        AddRigidbody();
     }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
     void OnParticleCollision(GameObject other)
     {
         // Debug.Log("Hit by: "+ other.gameObject.name);
         ProcessHit();
-        KillEnemy();
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }
     }
 
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
 
